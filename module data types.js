c9a43c85aedbd305166afd3099c000987ce29d32
Sergey Rychkov <rychkovwlk@gmail.com>
2022-07-22 12:17:45 +0300

    module Conditional Statements and Data Types

---
+++ module data types.js
@@ -0,0 +1,33 @@
+//module Conditional Statements and Data Types
+//data types
+let str = 'World'//string
+let num = 100;//number
+let  boolean = true;//boolean
+//addition
+console.log(str + num);
+console.log(str + boolean);
+console.log(num + boolean);
+//multiplication 
+console.log(str * num);
+console.log(str * boolean);
+console.log(num * boolean);
+//Dividing
+console.log(str / num);
+console.log(str / boolean);
+console.log(num / boolean);
+//conversation
+let bool = true;//string
+console.log(typeof bool);
+boolean = String(bool);
+console.log(typeof bool)
+let a = ('6' / '3')//number
+console.log(typeof a);
+let string = "123" ;
+console.log(typeof string);
+let strtonum = Number(string);
+console.log(typeof strtonum);
+//boolean
+console.log(Boolean(1))
+console.log(Boolean(0))
+console.log(Boolean(""))
+console.log(Boolean("Hello"))
