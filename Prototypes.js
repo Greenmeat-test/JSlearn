d0d03f12fd991bfb20284772d97fafb1a93b933b
Sergey Rychkov <rychkovwlk@gmail.com>
2022-07-24 18:23:55 +0300

    module 7

---
+++ Prototypes.js
@@ -0,0 +1,13 @@
+//Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
+class animal{
+constructor(name){
+    this.move = move;
+}
+
+}
+console.log(animal.move)
+
+class cat extends animal{
+
+}
+console.log(cat.move)
